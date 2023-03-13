1a3f3b16a5d4f6f280e5e8fb6bf2c8e48d643392
yao <1739072611@qq.com>
2023-03-13 19:43:38 +0800

    完成了个人中心功能的开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,54 @@
+$(function () {
+    var form = layui.form
+    var layer = layui.layer
+    // verify自定义规则
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1 ~ 6 个字符之间！'
+            }
+        }
+    })
+    initUserInfo()
+    // 初始化用户信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+                console.log(res)
+                //   调用form.val()为表单快速赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+    // 重置表单的数据
+    $('#btnReset').on('click', function(e) {
+        // 阻止表单的默认重置行为
+        e.preventDefault()
+        initUserInfo()
+      })
+
+    // 监听表单的提交事件
+      $('.layui-form').on('submit', function(e) {
+        // 阻止表单的默认提交行为
+        e.preventDefault() 
+        // 发起 ajax 数据请求
+        $.ajax({
+          method: 'POST',
+          url: '/my/userinfo',
+          data: $(this).serialize(),
+          success: function(res) {
+            if (res.status !== 0) {
+              return layer.msg('更新用户信息失败！')
+            }
+            layer.msg('更新用户信息成功！')
+            // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+            window.parent.getUserInfo()
+          }
+        })
+      })
+})
